6cd6cf5a29700804527a1ddf1bdb40981dd0ae2d
joseph townshend <joetownshend@mac.com>
2019-05-18 14:09:12 +0100

    first commit

---
+++ spec/greeter_spec.rb
@@ -0,0 +1,12 @@
+require 'greeter'
+
+describe 'Greeter' do
+  it 'greets Rico' do
+    expect(greet('Rico')).to eq 'Hello, Rico, how are you today?'
+  end
+  describe 'Greeter' do
+    it 'greets Chloe' do
+      expect(greet('Chloe')).to eq 'Hello, Chloe, how are you today?'
+    end
+  end
+end
